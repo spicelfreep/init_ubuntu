40ffbd9111ad8d127a6ceb77085839cac4e9f0b9
jackiexiao <707610215@qq.com>
2019-07-05 10:18:57 +0800

    更新shadowsocks

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -158,6 +158,20 @@ function tmux_install(){
 	#   prefix + Ctrl-r
 }
 
+function flameshot_install(){
+	# Because flameshot on ubuntu 18.04 only have 0.5 version, which is missing text feature...
+	sudo apt install g++ build-essential qt5-default qt5-qmake qttools5-dev-tools
+	sudo apt install libqt5dbus5 libqt5network5 libqt5core5a libqt5widgets5 libqt5gui5 libqt5svg5-dev
+	sudo apt install git openssl ca-certificates
+	git clone https://github.com/lupoDharkael/flameshot.git
+	cd flameshot
+	mkdir build
+	cd build
+	qmake ../
+	sudo make
+	sudo make install
+}
+
 function oh_my_zsh_install(){
 	sudo apt install zsh
 	sudo apt install curl
@@ -206,6 +220,12 @@ function conda_update_source(){
 	conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
 	conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
 	conda config --set show_channel_urls yes
+}
+
+function shadowsocks_install(){
+	# 这个感觉比pip install 来的方便多
+	sudo apt install shadowsocks-libev
+	# ss-local -c config.json
 }
 #function manual_install(){
 #	# Attention ! You should manally install !
